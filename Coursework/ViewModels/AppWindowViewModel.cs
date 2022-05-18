37261ffa7b0012b11ba9b29056243b805f56d5c5
денис иванов <denis1242009@gmail.com>
2022-05-18 19:29:44 +1000

    <Requred>

---
+++ Coursework/ViewModels/AppWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Coursework.Views.Pages;
+using System.Windows.Controls;
+
+namespace Coursework.ViewModels
+{
+    public sealed class AppWindowViewModel
+    {
+        public AppWindowViewModel()
+        {
+            ShowSettingsCommand = new(o =>
+                CurrentPage = new SettingsPage());
+        }
+
+        public Command ShowSettingsCommand { get; }
+
+        public Page? CurrentPage { get; set; }
+    }
+}
